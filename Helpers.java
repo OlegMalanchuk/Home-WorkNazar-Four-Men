cb6f0c3089bf879446bec4490b74ef141354a526
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-06-15 00:09:03 +0300

    Create Helpers.java

---
+++ Helpers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Helpers
+{
+//    public void information(){
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("first name : ");
+//        String data1 = scanner.nextLine();
+//        System.out.print("last name : ");
+//        String data2 = scanner.nextLine();
+//        System.out.print("foot size : ");
+//        String data3 = scanner.nextLine();
+//        System.out.println("end");
+//    }
+
+
+}
