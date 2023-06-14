df34b5ea0fe50e27599ecc887d0bf369b16c0b92
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-06-15 00:08:58 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Men men = new Men();
+        men.person1();
+        men.person2();
+        men.person3();
+        men.person4();
+
+
+    }
+}
