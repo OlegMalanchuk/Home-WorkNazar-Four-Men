13566afdbd43f54dfc2597665fd20d644ec5e9e5
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-06-16 15:23:41 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,47 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args)
     {
         Men men = new Men();
+        Cycle cycle = new Cycle();
+        Register register = new Register();
+
+
+
         men.person1();
-        men.person2();
-        men.person3();
-        men.person4();
+
+//        men.person2();
+//        men.person3();
+//        men.person4();
+
+        cycle.cyclyWhileTrue();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
+
+
 }
