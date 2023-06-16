298fb144201465d111c4fe1e145fb389f65e321e
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-06-16 15:38:21 +0300

    Create Men.java

---
+++ src/Men.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Men {
+
+    private String firstName,lastName ;//змінні
+    private int footSize;//змінні
+    Register register = new Register();//ініціалізація класу
+
+
+    public  void person1(){
+
+        HashMap<String, String> men1 = register.create1();//створення персонажів
+
+    }
+//    public  void person2(){
+//
+//        HashMap<String, String> men2 = register.create();//register.create() відсилаємося на клас register з методом create() функціонал розписано в методі
+//        }
+//    public  void person3(){
+//
+//        HashMap<String, String> men3 = register.create();
+//    }
+//    public  void person4(){
+//
+//        HashMap<String, String> men4 = register.create();
+//    }
+//    public void informOnPerson(){
+//        HashMap<String,String>men1
+//    }
+
+
+
+}
