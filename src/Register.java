a7c2bb10a2b8ec99f54c4c71b48478904a45951c
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-06-16 15:38:24 +0300

    Create Register.java

---
+++ src/Register.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Register {
+    String men1firstName;
+    String men1lastName;
+    String men1firstName;
+    public HashMap<String, String> create1(){
+        Helpers helpers = new Helpers();
+        HashMap<String, String> men1 = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("first name : ");
+        String data1 = scanner.nextLine();
+        System.out.print("last name : ");
+        String data2 = scanner.nextLine();
+        System.out.print("foot size : ");
+        Integer data3 = Integer.valueOf(scanner.nextLine());
+        helpers.out();
+
+        men1.put("firstName",data1);
+        men1.put("lastName",data2);
+        men1.put("footSize", String.valueOf(data3));
+        for (Map.Entry<String,String>e: men1.entrySet()){
+            System.out.println(e.getKey() +" = "+ e.getValue());
+        }
+        return men1;
+    }
+    private  HashMap<String, String> men1;
+
+    public void display( HashMap<String, String> men1){
+        // використовуємо цикл for-each для перебору всіх пар ключ-значення в HashMap
+        for (Map.Entry<String,String> e: men1.entrySet()){
+            // виводимо ключ і значення, розділяючи їх знаком рівності
+            System.out.println(e.getKey() +" = "+ e.getValue());
+        }
+    }
+
+
+}
