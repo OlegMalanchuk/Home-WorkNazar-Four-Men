ce4c1a06130ee770f892426c441f94d11ab82853
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-06-17 18:32:34 +0300

    Update Men.java

--- src/Men.java
+++ src/Men.java
@@ -1,47 +1,50 @@
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
 
-public class Men {
+public class Men
+{
+    private HashMap <String , String> men1;// це метод ключ-значення ніби
+    private HashMap <String , String> men2;// це метод ключ-значення ніби
+    private HashMap <String , String> men3;// це метод ключ-значення ніби
+    private HashMap <String , String> men4;// це метод ключ-значення ніби
+// це метод ключ-обєкт значиння
+    public HashMap<String,String> create(){
+        HashMap<String, String> men = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("first name : ");
+        men.put("firstName",scanner.next());// ввід даних і одночасно запис їх в хес мер
+        System.out.print("last name : ");
+        men.put("lastName",scanner.next());
+        System.out.print("foot size : ");
+        men.put("footSize", scanner.next());
+        Helpers.out();
 
-    private String firstName,lastName ;//змінні
-    private String footSize;//змінні
-    Register register = new Register();//ініціалізація класу
-    private HashMap<String,String> men1;
-    private HashMap<String,String> men2;
-    private HashMap<String,String> men3;
-    private HashMap<String,String> men4;
-
-    public void human(){
-        this.men1 = this.register.create();
-        this.men2 = this.register.create();
-        this.men3 = this.register.create();
-        this.men4 = this.register.create();
+        return men;// повертає змінну мен із записаними значеннями
     }
 
-    public  HashMap<String, String> getMen1() {return men1;}
-
-    public HashMap<String, String> getMen2() {
-        return men2;
+    public Men(){
+        this.men1=this.create();// констрктори  які приймають вище записані параметри кожен окремо та різні
+        this.men2=this.create();// констрктори  які приймають вище записані параметри кожен окремо та різні
+        this.men3=this.create();// констрктори  які приймають вище записані параметри кожен окремо та різні
+        this.men4=this.create();// констрктори  які приймають вище записані параметри кожен окремо та різні
     }
+    public  HashMap<String, String> getMen1() {return men1;} // банальні гетери
 
-    public HashMap<String, String> getMen3() {
-        return men3;
-    }
+    public HashMap<String, String> getMen2() {return men2;} // банальні гетери
 
-    public HashMap<String, String> getMen4() {
-        return men4;
-    }
+    public HashMap<String, String> getMen3() {return men3;} // банальні гетери
 
+    public HashMap<String, String> getMen4() {return men4;} // банальні гетери
 
 
-    public void setFootMen1(String foot) {
-        this.men1.put("footSize", foot);
-    }public void setFootMen2(String foot) {
-        this.men2.put("footSize", foot);
-    }public void setFootMen3(String foot) {
-        this.men3.put("footSize", foot);
-    }public void setFootMen4(String foot) {
-        this.men4.put("footSize", foot);
-    }
+
+    public void setFootMen1(String foot) {this.men1.put("footSize", foot);} // сетери які використовуються для зміни розміру пенсіла )
+    public void setFootMen2(String foot) {this.men2.put("footSize", foot);} // сетери які використовуються для зміни розміру пенсіла )
+    public void setFootMen3(String foot) {this.men3.put("footSize", foot);} // сетери які використовуються для зміни розміру пенсіла )
+    public void setFootMen4(String foot) {this.men4.put("footSize", foot);} // сетери які використовуються для зміни розміру пенсіла )
+
+
+
+
+
 }
-
