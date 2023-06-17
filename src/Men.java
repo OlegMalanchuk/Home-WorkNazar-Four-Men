2b502a517e05345c6d10accc70865e33c4b821d6
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-06-17 14:15:24 +0300

    Update Men.java

--- src/Men.java
+++ src/Men.java
@@ -1,35 +1,47 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Men {
 
     private String firstName,lastName ;//змінні
-    private int footSize;//змінні
+    private String footSize;//змінні
     Register register = new Register();//ініціалізація класу
+    private HashMap<String,String> men1;
+    private HashMap<String,String> men2;
+    private HashMap<String,String> men3;
+    private HashMap<String,String> men4;
 
-
-    public  void person1(){
-
-        HashMap<String, String> men1 = register.create1();//створення персонажів
-
+    public void human(){
+        this.men1 = this.register.create();
+        this.men2 = this.register.create();
+        this.men3 = this.register.create();
+        this.men4 = this.register.create();
     }
-//    public  void person2(){
-//
-//        HashMap<String, String> men2 = register.create();//register.create() відсилаємося на клас register з методом create() функціонал розписано в методі
-//        }
-//    public  void person3(){
-//
-//        HashMap<String, String> men3 = register.create();
-//    }
-//    public  void person4(){
-//
-//        HashMap<String, String> men4 = register.create();
-//    }
-//    public void informOnPerson(){
-//        HashMap<String,String>men1
-//    }
+
+    public  HashMap<String, String> getMen1() {return men1;}
+
+    public HashMap<String, String> getMen2() {
+        return men2;
+    }
+
+    public HashMap<String, String> getMen3() {
+        return men3;
+    }
+
+    public HashMap<String, String> getMen4() {
+        return men4;
+    }
 
 
 
+    public void setFootMen1(String foot) {
+        this.men1.put("footSize", foot);
+    }public void setFootMen2(String foot) {
+        this.men2.put("footSize", foot);
+    }public void setFootMen3(String foot) {
+        this.men3.put("footSize", foot);
+    }public void setFootMen4(String foot) {
+        this.men4.put("footSize", foot);
+    }
 }
+
