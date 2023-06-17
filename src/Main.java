0f2aee9868015661dc94a030f0ab88baaf22557a
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-06-17 18:32:30 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,5 @@
-import java.util.HashMap;
-import java.util.Scanner;
-
-public class Main
-{
-    public static void main(String[] args)
-    {
-        Men men = new Men();
-        Cycle cycle = new Cycle();
-        Register register = new Register();
-
-        men.human();
-        cycle.cyclyWhileTrue();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+public class Main {
+    public static void main(String[] args) {
+        Cycle.WhileTrue();// параша зараз розберемося )
     }
-
-
 }
