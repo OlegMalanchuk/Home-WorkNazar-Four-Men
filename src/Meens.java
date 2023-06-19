911ce66a6c6d3a5bfb44f42efba13756b9a1d12e
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-06-19 13:39:03 +0300

    Update Meens.java

--- src/Meens.java
+++ src/Meens.java
@@ -1,16 +1,16 @@
 import java.util.HashMap;
 
-public class Meens extends Men{
+public class Meens{
     private  HashMap<String , HashMap<String,String>> mens;
 
 
     public HashMap<String , HashMap<String,String>> createMeens()
     {
         HashMap<String , HashMap<String,String>> allMens = new HashMap<>();
-        allMens.put("men1",getMen1());
-        allMens.put("men2",getMen2());
-        allMens.put("men3",getMen3());
-        allMens.put("men4",getMen4());
+        allMens.put("men1",Men.getMen1());
+        allMens.put("men2",Men.getMen2());
+        allMens.put("men3",Men.getMen3());
+        allMens.put("men4",Men.getMen4());
         return allMens;
     }
     public Meens(){
