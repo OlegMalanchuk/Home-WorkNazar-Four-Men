258c494015679732ec1f3bee4784527e39a591eb
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-06-19 13:30:06 +0300

    Update Cycle.java

--- src/Cycle.java
+++ src/Cycle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Cycle
+public class Cycle extends Meens
 {
     public static void WhileTrue(){// це база програми де відбуваються всі дії
         Scanner question = new Scanner(System.in);
@@ -8,40 +8,89 @@ public class Cycle
 
         while (true){//цик безкінечний, виходу нема, чому, бо так, я так захотів
         Helpers.outQuestion();//хелпер там все пише
+
         int num = question.nextInt();//змінна яка відповідає на питання хелпера
-         if (num ==1)
+
+         if (num == 1)
          {
-             Helpers.choice();//хелпер там все пише
-             int mm = question.nextInt();//змінна яка відповідає на питання хелпера
+             Helpers.choiceFoot();//хелпер там все пише
+             int mm  = question.nextInt();//змінна яка відповідає на питання хелпера
 
-             if (mm == 1)
-             {
-                 System.out.print("введіть розмір першого: ");//тут ми замінюємо розмір ноги персонажа якого виберемо вище
-                 String foot = question.next();// створення та ініціалізація змінної
-                 men.setFootMen1(foot);//+ її до ключа-значення
+             switch (mm ){
+                 case 1:
+                     System.out.print("введіть розмір першого: ");//тут ми замінюємо розмір ноги персонажа якого виберемо вище
+                     String foot1 = question.next();// створення та ініціалізація змінної
+                     men.setFootMen1(foot1);//+ її до ключа-значення
+                     break;
+                 case 2:
+                     System.out.print("введіть розмір другого: ");//то саме
+                     String foot2 = question.next();
+                     men.setFootMen2(foot2);
+                     break;
+                 case 3:
+                     System.out.print("введіть розмір третьго: ");//то саме
+                     String foot3 = question.next();
+                     men.setFootMen3(foot3);
+                     break;
+                 case 4:
+                     System.out.print("введіть розмір четвертого: ");//то саме
+                     String foot4 = question.next();
+                     men.setFootMen4(foot4);
+                     break;
+             }
+         } else if (num == 2) {
+             Helpers.choiceNameF();
+             int kk = question.nextInt();
+             switch (kk){
+                 case 1:
+                     System.out.println("new name :");// прейменовування імені
+                     String name1 = question.next();
+                     men.setNameMen1(name1);
+                     break;
+                 case 2:
+                     System.out.println("new name :");
+                     String name2 = question.next();
+                     men.setNameMen2(name2);
+                     break;
+                 case 3:
+                     System.out.println("new name :");
+                     String name3 = question.next();
+                     men.setNameMen3(name3);
+                     break;
+                 case 4:
+                     System.out.println("new name :");
+                     String name4 = question.next();
+                     men.setNameMen4(name4);
+                     break;
+             }
 
-             }
-             if (mm==2)
-             {
-                 System.out.print("введіть розмір другого: ");//то саме
-                 String foot = question.next();
-                 men.setFootMen2(foot);
-             }
-             if (mm==3)
-             {
-                 System.out.print("введіть розмір третьго: ");//то саме
-                 String foot = question.next();
-                 men.setFootMen3(foot);
-             }
-             if (mm==4)
-             {
-                 System.out.print("введіть розмір четвертого: ");//то саме
-                 String foot = question.next();
-                 men.setFootMen4(foot);
-             }
          }
-         if (num==2)//якщо ви вибрали це тут виводиться інфа про всіх учасників
-         {
+         else if( num == 3){
+             Helpers.choiceNameL();
+             int ff = question.nextInt();
+             switch (ff){
+                 case 1:
+                     System.out.println("new lastName :");// перейменування прізвища, щоб Назарко не доєбався
+                     String name1 = question.next();
+                     men.setLastNameMen1(name1);
+                     break;
+                 case 2:
+                     System.out.println("new lastName :");
+                     String name2 = question.next();
+                     men.setLastNameMen2(name2);
+                     break;
+                 case 3:
+                     System.out.println("new lastName :");
+                     String name3 = question.next();
+                     men.setLastNameMen3(name3);
+                     break;
+                 case 4:
+                     System.out.println("new lastName :");
+                     String name4 = question.next();
+                     men.setLastNameMen4(name4);
+                     break;
+             }
+         } else if (num == 4) {//якщо ви вибрали це тут виводиться інфа про всіх учасників
              Helpers.displayMen(men.getMen1());// класна штука через те що метод в хелпері статичний мим можемо його викликати без
              // ініціалізації цілого класу, а він ту виконує дію яку я розписав в хелпері
              //коротко записуєш значення чоловіка якого ти хочеш вивести іт циклом воно тобі виводить всю інфу
@@ -52,22 +101,16 @@ public class Cycle
              Helpers.out();
              Helpers.displayMen(men.getMen4());
              Helpers.out();
+            Meens meens = new Meens();
+             Helpers.displayMens(meens.getMens());
+
+
          }
-
-    }
     }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+    }
 }
+
+
